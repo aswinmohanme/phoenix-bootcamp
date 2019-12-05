056f950c5cca90f63563468adb9240cfbddd0a75
aswinmohanme <dev.aswinmohan@gmail.com>
2019-12-05 11:50:21 +0530

    Create Topic form

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,10 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html")
+  end
+end
