a57e856805a931b62795e5ab4d09a9fa255d7d79
aswinmohanme <dev.aswinmohan@gmail.com>
2019-12-15 16:45:40 +0530

    Complete Signin and Signout Flow

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(Discuss.Plugs.SetUser)
   end
 
   pipeline :api do
@@ -23,7 +24,8 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through(:browser)
 
-    get("/:provider", AuthController, :index)
+    get("/signout", AuthController, :signout)
+    get("/:provider", AuthController, :request)
     get("/:provider/callback", AuthController, :callback)
   end
 
